2cba81ed8e6d544eb85099bdf854861426519250
liyang1384 <liyang1384@qq.com>
2020-10-31 19:27:48 +0800

    引入antd

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router).mount('#app')
 
 app.use(Antd);
