04b42620c246129d5e6977f8221d3d1141112b0b
liyang1384 <liyang1384@qq.com>
2020-11-28 23:28:43 +0800

    添加用户默认布局

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout>
+    <Header />
+    <router-view />
+  </a-layout>
+</template>
+
+<script>
+import Header from '@/layouts/Header.vue'
+
+export default {
+  name: 'UserLayout',
+  components: {
+    Header
+  }
+}
+</script>
