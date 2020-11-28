9da5426e585aa12f1d8d662f3b175c9274e289e0
liyang1384 <liyang1384@qq.com>
2020-11-28 23:28:13 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import UserLayout from '../layouts/UserLayout.vue'
+import OrderList from '../views/OrderList.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'UserLayout',
+    component: UserLayout,
+    children: [
+      {
+        path: 'orderlist',
+        name: 'OrderList',
+        component: OrderList
+      }
+    ]
   }
 ]
 
