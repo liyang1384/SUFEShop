3a7fad82bb543cec13dbe72d5f4144de3bd11dbf
sorted-List <71689514+sorted-List@users.noreply.github.com>
2020-12-06 19:05:52 +0800

    谢文龙的版本_0
    谢文龙的版本_0

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,49 @@ import HistoryList from '../views/HistoryList.vue'
 import MyFavourites from '../views/MyFavourites.vue'
 import CommodityDetail from '../views/CommodityDetail.vue'
 import CreateOrder from '../views/CreateOrder.vue'
-import UserView from '../views/UserView.vue'
-import MessageView from '../views/MessageView.vue'
+import RefundReview from '../views/RefundReview.vue'
+import OrderReview from '../views/OrderReview.vue'
+import RefundAppeal from '../views/RefundAppeal.vue'
+import ModifyCode from '../views/ModifyCode.vue'
+import OrderInformation from '../views/OrderInformation.vue'
+import PersonalPage from '../views/PersonalPage.vue'
+
 const routes = [
   {
     path: '/',
     name: 'UserLayout',
     component: UserLayout,
     children: [
+      {
+        path: 'personalpage',
+        name: 'PersonalPage',
+        component: PersonalPage
+      },
+      {
+        path: 'orderinformation',
+        name: 'OrderInformation',
+        component: OrderInformation
+      },
+      {
+        path: 'modifycode',
+        name: 'ModifyCode',
+        component: ModifyCode
+      },
+      {
+        path: 'refundappeal',
+        name: 'RefundAppeal',
+        component: RefundAppeal
+      },
+      {
+        path: 'orderreview',
+        name: 'OrderReview',
+        component: OrderReview
+      },
+      {
+        path: 'refundreview',
+        name: 'RefundReview',
+        component: RefundReview
+      },
       {
         path: 'orderlist',
         name: 'OrderList',
@@ -37,16 +72,6 @@ const routes = [
         path: 'createorder',
         name: 'CreateOrder',
         component: CreateOrder
-      },
-      {
-        path: 'userview',
-        name: 'userview',
-        component: UserView
-      },
-      {
-        path: 'messageview',
-        name: 'messageview',
-        component: MessageView
       }
     ]
   }
