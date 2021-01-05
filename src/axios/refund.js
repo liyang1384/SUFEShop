567a88a166c3047b52133f1b63d95537bd880b03
sortedList <862122250@qq.com>
2021-01-05 20:29:14 +0800

    ""

--- src/axios/refund.js
+++ src/axios/refund.js
@@ -1,10 +1,35 @@
 import instance from './index.js'
 
-export function getRefundDetail (data) {
+export function getRefundDetail_char(data) {
+  return instance({
+    url: '/refunddetail',
+    // 请求方法：get
+    method: 'get'
+  })
+}
+
+export function postRefundDetail_char(data) {
   return instance({
     url: '/refunddetail',
     // 请求方法：get
     method: 'post',
+    data
+  })
+}
+
+export function getRefundDetail_img(data) {
+  return instance({
+    url: '/refunddetail',
+    method: 'get',
+    responseType: 'stream'
+  })
+}
+
+export function postRefundDetail_img(data) {
+  return instance({
+    url: '/refunddetail',
+    method: 'post',
+    responseType: 'stream',
     data
   })
 }
