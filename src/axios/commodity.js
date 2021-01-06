012252b516a05dc1843433b6740f4585376a5ed7
lynette-1 <1216678249@qq.com>
2021-01-06 17:07:46 +0800

    新增 获取指定商品信息明细

--- src/axios/commodity.js
+++ src/axios/commodity.js
@@ -8,3 +8,13 @@ export function getAuditCommodityList (params) {
     params: params
   })
 }
+
+export function getCommodity (params) {
+  return instance({
+    url: 'commodity/CommodityDetail/',
+    // 请求方法：get
+    method: 'get',
+    params: params
+  })
+}
+//获取指定商品信息明细
